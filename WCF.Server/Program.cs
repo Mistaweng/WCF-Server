442bac7faa4258749b3202a38f03679892f220b6
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-09-18 21:22:45 +0100

    Add project files.

---
+++ WCF.Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.Server
+{
+	internal class Program
+	{
+		[ServiceContract]
+		public interface IMessageService
+		{
+			string[] GetMessages();
+		}
+		static void Main(string[] args)
+		{
+		}
+	}
+}
