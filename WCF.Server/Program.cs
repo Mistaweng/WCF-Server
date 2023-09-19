ea70ab4e38ad6d17572d9a395316cc300cf6cde0
Mistaweng <122454766+Mistaweng@users.noreply.github.com>
2023-09-19 22:43:16 +0100

    Added two project with different serveres

--- WCF.Server/Program.cs
+++ WCF.Server/Program.cs
@@ -12,10 +12,36 @@ namespace WCF.Server
 		[ServiceContract]
 		public interface IMessageService
 		{
+			[OperationContract]
 			string[] GetMessages();
 		}
+
+		[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+		public class MessageService : IMessageService
+		{
+			public string[] GetMessages()
+			{
+				return new string[] { "server1", "server2", "server3" };
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			var uris = new Uri[1];
+			string address = "net.tcp://localhost:6565/MessageService";
+			uris[0] = new Uri(address);
+			IMessageService message = new MessageService();
+			ServiceHost host = new ServiceHost(message, uris);
+			var binding = new NetTcpBinding(SecurityMode.None);
+			host.AddServiceEndpoint(typeof(IMessageService), binding, "");
+			host.Opened += Host_Opened;
+			Console.ReadKey();
+
 		}
+
+		private static void Host_Opened(object sender, EventArgs e)
+		{
+            Console.WriteLine("message service started");
+        }
 	}
 }
